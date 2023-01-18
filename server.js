e13693a7645971928a73f863989e6bda1f310be0
Josue-2021 <jgonzalez@fusionhit.com>
2023-01-18 14:50:10 -0600

    Texto modificado

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',(req,res) => res.send('Hello world with express!'))
+app.get('/',(req,res) => res.send('<h1>Pruebas con aws</h1>'))
 
 
 app.listen(3000);
